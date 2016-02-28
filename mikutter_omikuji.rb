ea4072159644cc4428a733b782fe38c856270e66
Atsushi TAMANO <Asyley13@gmail.com>
2016-02-28 22:32:00 +0900

    Update mikutter_omikuji.rb

--- mikutter_omikuji.rb
+++ mikutter_omikuji.rb
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-Plugin.create :mikutter_omikuji do
+Plugin.create :mikutter_reply do
   on_appear do |ms|
     ms.each do |m|
 
@@ -8,7 +8,14 @@ Plugin.create :mikutter_omikuji do
       end
 
       if m.message.to_s =~ /^@#{Service.primary.user.to_s} おみくじ/
+        m.favorite
         Service.primary.post :message => "@#{m.user.to_s} マジキチ", :replyto => m.message
+      elsif m.message.to_s =~ /^@#{Service.primary.user.to_s} ACODC/
+        m.favorite
+        Service.primary.post :message => "@#{m.user.to_s} アリス・カータレットちゃんのおぱんつドリップコーヒー", :replyto => m.message
+      elsif m.message.to_s =~ /^@#{Service.primary.user.to_s} SCODC/
+        m.favorite
+        Service.primary.post :message => "@#{m.user.to_s} シャロちゃんのおぱんつドリップコーヒー", :replyto => m.message
       end
     end
   end
